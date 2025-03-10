cf56e01432613b72b0ca464be7d10ff2780ffd06
Vlemrc <lemerciervictor@gmail.com>
2025-03-10 15:28:15 +0100

    noteitem style when ontop

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -5,10 +5,11 @@ interface NoteItemProps {
   note: Note
   isActive: boolean
   nextIsActive?: boolean
+  isTopNote?: boolean
   onClick: () => void
 }
 
-const NoteItem = ({ note, isActive, nextIsActive, onClick }: NoteItemProps) => {
+const NoteItem = ({ note, isActive, nextIsActive, isTopNote, onClick }: NoteItemProps) => {
   // Formatage de la date
   const date = new Date(note.createdAt).toLocaleDateString("fr-FR", {
     day: "2-digit",
@@ -17,7 +18,10 @@ const NoteItem = ({ note, isActive, nextIsActive, onClick }: NoteItemProps) => {
   })
 
   return (
-    <div onClick={onClick} className={`cursor-pointer ${isActive || nextIsActive ? "" : "border-nav-note"}`}>
+    <div
+      onClick={onClick}
+      className={`cursor-pointer ${isActive || nextIsActive ? "" : "border-nav-note"} ${isTopNote ? "mt-3" : ""}`}
+    >
       <li className={`${isActive ? "bg-gray" : "bg-white"} pt-4 pl-7 w-full rounded-md transition-colors`}>
         <h6 className="font-black text-sm text-left">{note.title}</h6>
         <div className="flex flex-row gap-2.5 pb-4 pr-7">
